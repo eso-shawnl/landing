c9e9dae220154aee320478882c373366b754edcc
Shawn Lin <jallen0927@hotmail.com>
2015-05-02 12:30:38 +1200

    basically done

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,62 @@
 ;(function($){
     var $overlay = $(".primary-section .panel-overlay"),
         $starter = $("#starter-button"),
-        $section2 = $("#secondary-section");
+        $section1 = $("#primary-section"),
+        $section2 = $("#secondary-section"),
+        $upButton = $("#secondary-section .button-heavy"),
+        $info = $("#information");
+
     $(window).on('mousewheel',
         function(e) {
-            console.log('test');
             e.preventDefault();
             e.stopPropagation();
         }
     );
 
     $starter.click(function(){
-        $('html, body').animate({
-           scrollTop: $section2.offset().top
-        }, 1200);
+        scrollDown();
     });
 
+    $upButton.click(function(){
+        scrollUp();
+    });
+
+    function scrollDown() {
+        $('html, body').animate({
+            scrollTop: $section2.offset().top
+        }, 1000, "linear", function(){
+            window.setTimeout(showInfo(), 1000);
+        });
+    }
+
+    function scrollUp() {
+        $('html, body').animate({
+                scrollTop: $section1.offset().top
+            },
+            800,
+            'linear',
+            function(){
+                resetPage();
+            }
+        );
+
+    }
+
+    function showInfo() {
+        $info.animate({'opacity': '1', 'top': '0'}, 2000, function(){
+            $upButton.animate({'opacity': '1'}, 500);
+        });
+    }
+
+    function resetPage() {
+        $info.css({'opacity': '0', 'top': '5em'});
+        $upButton.css({'opacity': '0'});
+        $('html, body').animate({
+            scrollTop: $section1.offset().top
+        });
+    }
+
+    $(window).load(function(){
+        resetPage();
+    });
 })(jQuery);
